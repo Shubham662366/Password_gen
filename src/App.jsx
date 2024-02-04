7de3f798d790aedfeb7f1798cb86fe5b4876a61e
demo learn <skumarsingh6623@gmail.com>
2024-02-04 15:35:30 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const[length,setlength] = useState(6);
+const[numberAllowled,setnumberAllowled] = useState(false);
+const[charAllowled,setcharAllowled] = useState(false);
+
+const[password,setpaaword] = useState("");
+
+const passwordRef = useRef(null);
+
+
+const passwordGenerator = useCallback( () => {
+    let pass ="";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if(numberAllowled) str += "0123456789";
+    if(charAllowled) str += "!?/~{}[]().,";
+
+    for(let i=1;i<=length;i++){
+        let char = (Math.floor(Math.random()*str.length + 1))
+    
+    pass += str.charAt(char);
+    }
+    setpaaword(pass);},
+    [length,numberAllowled,charAllowled,setpaaword])
+
+    useEffect(() => {
+        passwordGenerator()
+    },[length,passwordGenerator,numberAllowled,charAllowled])
+
+    const copyPasword = useCallback(() => {
+      passwordRef.current?.select();
+       window.navigator.clipboard.writeText(password);
+    },[password])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div id='warraper'> 
+    <div className='container '>
+          <h1 className='text-4xl text-centre'>Password Generator</h1>
+          <div className='display'>
+                <input type='text' placeholder='password' value={password} ref={passwordRef}/>
+                <button className='copy' onClick={copyPasword}>Copy</button>
+                
+          </div>
+          <div className='data'>
+           <div className='rangee'>
+             <input type='range' min={6} max={20} readOnly value={length}
+              onChange={(e) => {setlength(e.target.value)}}></input>
+              <label>length:{length}</label>
+            </div>
+           
+           <div className='checkBoxx'>
+                  <input type='checkbox' defaultChecked={numberAllowled} id='numberId'
+                  onChange={() =>{setnumberAllowled((prev) => !prev); }}></input>
+                  <label>Number</label>
+           </div>
+
+           <div className='checkBoxx1'>
+                  <input type='checkbox' defaultChecked={charAllowled} id='charId'
+                  onChange={() =>{setcharAllowled((prev) => !prev); }}></input>
+                  <label>Charater</label>
+           </div>
+
+
+          </div>
+    </div>
+    </div>
     </>
   )
 }
